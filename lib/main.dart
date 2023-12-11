4ff231837629c54de07ab364dff97c4851b0cf65
muhamadfarrelrizqullah <farrelswag27@gmail.com>
2023-12-11 18:14:20 +0700

    W12 P8

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:async/async.dart';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
-import 'package:books/geolocation.dart';
+import 'package:books/navigation_first.dart';
 
 void main() {
   runApp(const MainApp());
@@ -19,7 +19,7 @@ class MainApp extends StatelessWidget {
       theme: ThemeData(
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity),
-      home: const LocationScreen(),
+      home: const NavigationFirst(),
     );
   }
 }
