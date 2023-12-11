9f38cc96e307397c3b95a5473083760809580607
muhamadfarrelrizqullah <farrelswag27@gmail.com>
2023-12-11 17:16:30 +0700

    W12 P2

--- lib/main.dart
+++ lib/main.dart
@@ -43,14 +43,7 @@ class _FuturePageState extends State<FuturePage> {
           ElevatedButton(
             child: const Text('Go!'),
             onPressed: () {
-              setState(() {});
-              getData().then((value) {
-                result = value.body.toString().substring(0, 450);
-                setState(() {});
-              }).catchError((_) {
-                result = 'An error occured';
-                setState(() {});
-              });
+              count();
             },
           ),
           const Spacer(),
@@ -61,6 +54,31 @@ class _FuturePageState extends State<FuturePage> {
         ]),
       ),
     );
+  }
+
+  Future<int> returnOneAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 1;
+  }
+
+  Future<int> returnTwoAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 2;
+  }
+
+  Future<int> returnThreeAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 3;
+  }
+
+  Future count() async {
+    int total = 0;
+    total = await returnOneAsync();
+    total += await returnTwoAsync();
+    total += await returnThreeAsync();
+    setState(() {
+      result = total.toString();
+    });
   }
 }
 
